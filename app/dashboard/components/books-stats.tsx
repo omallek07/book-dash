872db65b0f073b67f0aeb09bd649b120b89de428
Kevin O'Malley <omallek@gmail.com>
2025-12-03 09:13:48 -0500

    Add book stat cards

---
+++ app/dashboard/components/books-stats.tsx
@@ -0,0 +1,98 @@
+import { Avatar, AvatarFallback } from '@/components/ui/avatar';
+import { Button } from '@/components/ui/button';
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+  CardFooter,
+} from '@/components/ui/card';
+import { BookIcon, BookDownIcon, BookUpIcon, UserIcon } from 'lucide-react';
+import Link from 'next/link';
+
+export default function BookStats() {
+  const totalBooks = 100;
+  const BooksRead = 70;
+  const booksReadPercentage = (BooksRead / totalBooks) * 100;
+
+  return (
+    <div className='grid lg:grid-cols-3 gap-4'>
+      <Card>
+        <CardHeader className='pb-2'>
+          <CardTitle className='text-base'>Total books</CardTitle>
+        </CardHeader>
+        <CardContent className='flex justify-between items-center'>
+          <div className='flex gap-2'>
+            <UserIcon size={24} />
+            <div className='text-5xl font-bold'>{totalBooks}</div>
+          </div>
+          <div>
+            <Button size='xs' asChild>
+              <Link href='dashboard/books'>View all</Link>
+            </Button>
+          </div>
+        </CardContent>
+      </Card>
+
+      <Card>
+        <CardHeader className='pb-2'>
+          <CardTitle className='text-base'>Completed books</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <div className='flex gap-2'>
+            {booksReadPercentage > 75 ? (
+              <BookUpIcon />
+            ) : booksReadPercentage > 50 ? (
+              <BookIcon />
+            ) : (
+              <BookDownIcon />
+            )}
+            <div className='text-5xl font-bold'>
+              {BooksRead} out of {totalBooks}
+            </div>
+          </div>
+        </CardContent>
+        <CardFooter>
+          {booksReadPercentage > 75 ? (
+            <span className='text-xs text-green-500 flex gap-1 items-center'>
+              <BookUpIcon />
+              {booksReadPercentage.toFixed(2)}% completed
+            </span>
+          ) : booksReadPercentage > 50 ? (
+            <span className='text-xs text-yellow-500 flex gap-1 items-center'>
+              <BookIcon />
+              Only {booksReadPercentage.toFixed(2)}% completed
+            </span>
+          ) : (
+            <span className='text-xs text-red-500 flex gap-1 items-center'>
+              <BookDownIcon />
+              Just {booksReadPercentage.toFixed(2)}% completed
+            </span>
+          )}
+        </CardFooter>
+      </Card>
+
+      <Card className='border-pink-500 flex flex-col'>
+        <CardHeader className='pb-2'>
+          <CardTitle className='text-base'>Book of the month</CardTitle>
+        </CardHeader>
+        <CardContent className='flex-col gap-2 items-center'>
+          <div>
+            <span className='text-2xl'>East of Eden</span>
+          </div>
+          <div>
+            <span className='text-xs text-muted-foreground'>
+              by John Steinbeck
+            </span>
+          </div>
+        </CardContent>
+        <CardFooter className='flex gap-2 items-center text-muted-foreground mt-auto'>
+          <span className='text-xs text-pink-500 flex gap-1 items-center'>
+            <BookIcon />
+            Recommended for you
+          </span>
+        </CardFooter>
+      </Card>
+    </div>
+  );
+}
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,22 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+
+import BookStats from './components/books-stats';
+
 export default function DashboardPage() {
-  return <div>dashboard page</div>;
+  return (
+    <Tabs defaultValue='all-books'>
+      <TabsList className='mb-4'>
+        <TabsTrigger value='all-books'>All Books</TabsTrigger>
+        <TabsTrigger value='my-books'>My Books</TabsTrigger>
+      </TabsList>
+      <TabsContent value='all-books'>
+        <BookStats />
+      </TabsContent>
+      <TabsContent value='my-books'>
+        <div>
+          <h2>My Books</h2>
+        </div>
+      </TabsContent>
+    </Tabs>
+  );
 }
